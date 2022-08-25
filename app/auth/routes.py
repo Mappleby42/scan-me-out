ee46a6eb6553e8b54a0e8275d04ce6587dff2ebe
MatthewAppleby <MatthewAppleby@bitbucket.org>
2022-08-25 12:14:49 +0100

    Add basic employee CRUD

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -3,10 +3,11 @@ from app.auth.forms import RegistrationForm, LoginForm
 from app.auth import authentication
 from app.auth.models import User
 from flask_login import login_user, logout_user, login_required, current_user
+import sqlite3 as sql
+import requests
 
 @authentication.route('/register', methods=['GET','post'])
 def register_user():
-
     if current_user.is_authenticated:
         flash('You are already logged in.')
         return redirect(url_for('authentication.homepage'))
@@ -25,7 +26,12 @@ def register_user():
 
 @authentication.route('/')
 def index():
-    return render_template('index.html')
+    con=sql.connect("instance/db_web.db")
+    con.row_factory=sql.Row
+    cur=con.cursor()
+    cur.execute("select * from users")
+    data=cur.fetchall()
+    return render_template('index.html',datas=data)
 
 @authentication.route('/login', methods=['GET','POST'])
 def log_in_user():
@@ -45,8 +51,14 @@ def log_in_user():
     return render_template('login.html', form=form)
 
 @authentication.route('/homepage')
+@login_required
 def homepage():
-    return render_template('homepage.html')
+    con=sql.connect("instance/db_web.db")
+    con.row_factory=sql.Row
+    cur=con.cursor()
+    cur.execute("select * from users WHERE ADMIN_ID = '" + str(current_user.id) + "'")
+    data=cur.fetchall()
+    return render_template('homepage.html',datas=data)
 
 
 @authentication.route('/logout', methods=['GET'])
@@ -56,6 +68,74 @@ def log_out_user():
     logout_user()
     return redirect(url_for('authentication.log_in_user'))
 
+@authentication.route("/add_user",methods=['POST','GET'])
+@login_required
+def add_user():
+    if request.method=='POST':
+        con=sql.connect("instance/db_web.db")
+        cur=con.cursor()
+
+        uname=request.form['uname']
+        cur.execute("select * from users WHERE UNAME='" + uname + "'")
+        data=cur.fetchall()
+        if len(data)!=0:
+            return render_template("add_user.html", error="Error: That username is taken")
+
+        contact=request.form['contact']
+        cur.execute("select * from users WHERE CONTACT='" + contact + "'")
+        data=cur.fetchall()
+        if len(data)!=0:
+            return render_template("add_user.html", error="Error: That email address is taken")
+
+        code=""
+        unique_code = False
+        while not unique_code:
+            code = requests.get("https://www.passwordrandom.com/query?command=password").text
+            cur.execute("select * from users WHERE CODE='" + code + "'")
+            data=cur.fetchall()
+            if len(data)==0:
+                unique_code = True
+        
+        cur.execute("insert into users(UNAME,CONTACT,CODE,ADMIN_ID,ONSITE) values (?,?,?,?,?)",(uname,contact,code,current_user.id,False))
+        con.commit()
+        flash('User Added','success')
+        return redirect(url_for("authentication.homepage"))
+    return render_template("add_user.html")
+
+@authentication.route("/edit_user/<string:uid>",methods=['POST','GET'])
+@login_required
+def edit_user(uid):
+    if request.method=='POST':
+        uname=request.form['uname']
+        contact=request.form['contact']
+        onsite=True
+        try:
+            request.form['onsite']
+        except:
+            onsite=False
+        con=sql.connect("instance/db_web.db")
+        cur=con.cursor()
+        cur.execute("update users set UNAME=?,CONTACT=?,ONSITE=? where UID=?",(uname,contact,onsite,uid))
+        con.commit()
+        flash('User Updated','success')
+        return redirect(url_for("authentication.index"))
+    con=sql.connect("instance/db_web.db")
+    con.row_factory=sql.Row
+    cur=con.cursor()
+    cur.execute("select * from users where UID=?",(uid,))
+    data=cur.fetchone()
+    return render_template("edit_user.html",datas=data)
+
+    
+@authentication.route("/delete_user/<string:uid>",methods=['GET'])
+def delete_user(uid):
+    con=sql.connect("instance/db_web.db")
+    cur=con.cursor()
+    cur.execute("delete from users where UID=?",(uid,))
+    con.commit()
+    flash('User Deleted','warning')
+    return redirect(url_for("authentication.index"))
+    
 @authentication.app_errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
