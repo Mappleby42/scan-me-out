4e9f51f0076f040f65da44c249d16a885107bfbf
MatthewAppleby <MatthewAppleby@bitbucket.org>
2022-09-09 13:02:37 +0100

    modify database setup to come from run.py

--- run.py
+++ run.py
@@ -1,5 +1,33 @@
 from app import create_app, db
 from app.auth.models import User
+import sqlite3 as sql
+
+#connect to SQLite
+con = sql.connect('instance/db_web.db')
+
+#Create a Connection
+cur = con.cursor()
+
+#Drop users table if already exsist.
+cur.execute("DROP TABLE IF EXISTS users")
+
+#Create users table  in db_web database
+sql ='''CREATE TABLE "users" (
+	"UID"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"UNAME"	TEXT,
+	"CODE"	TEXT,
+	"ADMIN_ID"	INTEGER,
+	"ONSITE"	BOOLEAN,
+	"CONTACT"	TEXT
+)'''
+cur.execute(sql)
+
+#commit changes
+con.commit()
+
+#close the connection
+con.close()
+
 
 #if __name__ == '__main__':
 flask_app = create_app('prod')
